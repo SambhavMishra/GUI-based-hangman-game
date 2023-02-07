50dc1c3d0c203e8a912a7094d221364de00423cb
Sambhav <mishrasa_3@rknec.edu>
2023-02-07 22:10:10 +0530

    Currently cannot reflect guess properly

--- static/script.js
+++ static/script.js
@@ -4,6 +4,8 @@ let guessed
 
 let isLetter;
 
+
+
 try {
     const LETTER_EXPRESSION = /^\p{L}$/u; // Supported by ES6+, Some bugs in FF < 78
 
@@ -19,17 +21,15 @@ try {
     };
 }
 
-// let play = function(guessWord) {
-//   let container = document.getElementById("container");
 
-//   // let guessArea = document.createElement("div")
-//   // guessArea.id = "guessArea"
-//   // container.append(guessArea)
-
-//   container.style.display = "none"
-//   game = document.getElementById("gameBox")
-//   game.style.display = "block"
-// }
+let replace_ = function(original, replace, index, end) {
+  let first = original.substr(0,index);
+  let last = original.substr(index+1, end);
+  let newString = first + replace + last;
+  console.log(`First: ${first} Replace: ${replace} Index: ${index} Last: ${last}`);
+  console.log(`Newstring: ${newString}`)
+  return newString;
+}
 
 
 let play = function(guessWord) {
@@ -47,31 +47,32 @@ let play = function(guessWord) {
   guessArea.id = "guessArea"
   container.append(guessArea)
 
-  guessArea.innerHTML = guessWord
+  // guessArea.innerHTML = guessWord
 
+  let ans = document.createElement("div");
+  ans.id = "ans";
+  game.append(ans)
   game.append(guessArea)
   guessed = "_".repeat(guessWord.length).split("").join(" ")
-  guessArea.innerHTML = `<h2>${guessed}</h2>`
+  ans.innerHTML = `<h2>${guessed}</h2>`
   console.log(guessWord)
 
-  // i = 0
-  // while (i < guessWord.length){
-  //   document.addEventListener('keydown', function(event) {
-  //     console.log('Key pressed: ', event.key);
-  // //     guessArea.innerHTML += event.key
-  // //     guessed += event.key
-  //     i = i + 1;
-  //   });
-  // }
-
   let i = 0;
+  let chances = guessWord.length + 5;
     document.addEventListener('keydown', function(event) {
       key = event.key;
-      if (isLetter(key) && i < guessWord.length) {
+      if (isLetter(key) && i < chances) {
         console.log("key pressed : ", event.key)
-        guessArea.innerHTML += event.key ;
+        guessArea.innerHTML = event.key ;
         guessed += event.key;
+        // guessArea.innerHTML +=  guessWord.indexOf(key);
         i = i + 1;
+        console.log(guessWord.includes(key));
+        if (guessWord.includes(key)) {
+          end = guessWord.length-1
+          guessed = replace_(guessed, key, guessWord.indexOf(key), end);
+          ans.innerHTML = guessed;
+        }
       }
     })
   console.log(guessed)
