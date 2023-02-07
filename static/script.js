edbe6783cfc00f3f537d2465f81b577eb6175401
Sambhav <mishrasa_3@rknec.edu>
2023-02-07 15:12:35 +0530

    Now the letters can be read properly

--- static/script.js
+++ static/script.js
@@ -2,6 +2,23 @@
 let guessWord
 let guessed
 
+let isLetter;
+
+try {
+    const LETTER_EXPRESSION = /^\p{L}$/u; // Supported by ES6+, Some bugs in FF < 78
+
+    isLetter = (character) => {
+        return character && LETTER_EXPRESSION.test(character);
+    };
+} catch(e) {
+  	console.log('Unicode flag in regular expressions are not supported. Used legacy isLetter() function.');
+
+    isLetter = (character) => {
+      	// it doesn't work for some alphabets but still enough in most cases
+        return character && character.length === 1 && character.toLowerCase() !== character.toUpperCase();
+    };
+}
+
 // let play = function(guessWord) {
 //   let container = document.getElementById("container");
 
@@ -30,25 +47,34 @@ let play = function(guessWord) {
   guessArea.id = "guessArea"
   container.append(guessArea)
 
-  // guessArea.innerHTML = guessWord
+  guessArea.innerHTML = guessWord
 
   game.append(guessArea)
   guessed = "_".repeat(guessWord.length).split("").join(" ")
   guessArea.innerHTML = `<h2>${guessed}</h2>`
   console.log(guessWord)
 
-  i = 0
-  while (i < guessWord.length){
+  // i = 0
+  // while (i < guessWord.length){
+  //   document.addEventListener('keydown', function(event) {
+  //     console.log('Key pressed: ', event.key);
+  // //     guessArea.innerHTML += event.key
+  // //     guessed += event.key
+  //     i = i + 1;
+  //   });
+  // }
 
+  let i = 0;
     document.addEventListener('keydown', function(event) {
-      console.log('Key pressed: ', event.key);
-      guessArea.innerHTML += event.key
-      guessed += event.key
-    });
-  }
+      key = event.key;
+      if (isLetter(key) && i < guessWord.length) {
+        console.log("key pressed : ", event.key)
+        guessArea.innerHTML += event.key ;
+        guessed += event.key;
+        i = i + 1;
+      }
+    })
   console.log(guessed)
-
-
 }
 
 
